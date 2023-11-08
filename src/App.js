88dca46d4c04a130b74c362c3d2650cd297449b6
Sujeeth <sujeeth1807@gmail.com>
2023-11-08 06:50:07 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Container from './components/Container.js';
+import './components/Container.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Container />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Container.js
@@ -0,0 +1,14 @@
+import UserTable from './UserTable.js';
+import './UserTable.css';
+
+function Container()
+{
+    return (
+        <div className="container">
+            <h1>Dummy data</h1>
+            <UserTable />
+        </div>
+    )
+}
+
+export default Container;
